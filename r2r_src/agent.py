correct object concat average h&e

r2r_src/agent.py
@@ -303,7 +303,7 @@ class Seq2SeqAgent(BaseAgent):
                 features[i, :, :] = ob['feature']   # Image feat
             return Variable(torch.from_numpy(features), requires_grad=False).cuda()
 
-    def _candidate_variable(self, obs):
+    def _candidate_variable(self, obs, outputObj=False):
         candidate_leng = [len(ob['candidate']) + 1 for ob in obs]       # +1 is for the end
         candidate_feat = np.zeros((len(obs), max(candidate_leng), self.feature_size + args.angle_feat_size), dtype=np.float32)
         # Note: The candidate_feat at len(ob['candidate']) is the feature for the END

r2r_src/model.py
@@ -659,7 +659,7 @@ class SpeakerEncoder(nn.Module):
         self.post_lstm = nn.LSTM(self.hidden_size, self.hidden_size // self.num_directions, self.num_layers,
                                  batch_first=True, dropout=dropout_ratio, bidirectional=bidirectional)
 
-    def forward(self, action_embeds, feature, lengths, already_dropfeat=False):
+    def forward(self, action_embeds, feature, lengths, already_dropfeat=False, objMask=None, objFeat=None):
         """
         :param action_embeds: (batch_size, length, 2052). The feature of the view
         :param feature: (batch_size, length, 36, 2052). The action taken (with the image feature)
@@ -678,6 +678,7 @@ class SpeakerEncoder(nn.Module):
         batch_size, max_length, _ = ctx.size()
         if not already_dropfeat:
             feature[..., :-args.angle_feat_size] = self.drop3(feature[..., :-args.angle_feat_size])   # Dropout the image feature
+            
         x, _ = self.attention_layer(                        # Attend to the feature map
             ctx.contiguous().view(-1, self.hidden_size),    # (batch, length, hidden) --> (batch x length, hidden)
             feature.view(batch_size * max_length, -1, self.feature_size),        # (batch, length, # of images, feature_size) --> (batch x length, # of images, feature_size)
@@ -707,7 +708,7 @@ class SpeakerDecoder(nn.Module):
             nn.Linear(128, 1)
         )
 
-    def forward(self, words, ctx, ctx_mask, h0, c0):
+    def forward(self, words, ctx, ctx_mask, h0, c0, objMask=None, objFeat=None):
         embeds = self.embedding(words)
         embeds = self.drop(embeds)
         x, (h1, c1) = self.lstm(embeds, (h0, c0))

r2r_src/speaker.py
@@ -173,6 +173,9 @@ class Speaker():
         length = np.zeros(len(obs), np.int64)
         img_feats = []
         can_feats = []
+        obj_feats = []
+        obj_lens = []
+        max_obj_len = 0
         first_feat = np.zeros((len(obs), self.feature_size+args.angle_feat_size), np.float32)
         for i, ob in enumerate(obs):
             first_feat[i, -args.angle_feat_size:] = utils.angle_feature(ob['heading'], ob['elevation'])
@@ -181,7 +184,14 @@ class Speaker():
             if viewpoints is not None:
                 for i, ob in enumerate(obs):
                     viewpoints[i].append(ob['viewpoint'])
-            img_feats.append(self.listener._feature_variable(obs))
+            if args.denseObj:
+                obj_f, obj_l, img_f = self.listener._feature_variable(obs)
+                max_obj_len = max(max_obj_len, max(obj_l))
+                img_feats.append(img_f)
+                obj_lens.append(obj_l)
+                obj_feats.append(obj_f)
+            else:
+                img_feats.append(self.listener._feature_variable(obs))
             teacher_action = self._teacher_action(obs, ended)
             teacher_action = teacher_action.cpu().numpy()
             for i, act in enumerate(teacher_action):
@@ -192,10 +202,21 @@ class Speaker():
             length += (1 - ended)
             ended[:] = np.logical_or(ended, (teacher_action == -1))
             obs = self.env._get_obs()
+        obj_mask = []
+        if obj_lens is not None:
+            for obj_l in obj_lens:
+                obj_mask.append(utils.traj_length2mask(obj_l, max_obj_len))
         img_feats = torch.stack(img_feats, 1).contiguous()  # batch_size, max_len, 36, 2052
         can_feats = torch.stack(can_feats, 1).contiguous()  # batch_size, max_len, 2052
+        obj_pad_feats = torch.zeros((img_feats(0),img_feats(1),max_obj_len, obj_f.size(-1)))
+        # for i,obj_l in enumerate(obj_lens):
+            # obj_l[:,:,k,:] =
+        # obj_feats = torch.stack(obj_feats, 1).contiguous()
+        # obj_lens = torch.stack()
         if get_first_feat:
             return (img_feats, can_feats, first_feat), length
+        elif args.denseObj:
+            return (img_feats, can_feats, obj_lens,obj_feats),length
         else:
             return (img_feats, can_feats), length
 
@@ -224,11 +245,22 @@ class Speaker():
         else:
             obs = self.env._get_obs()
             batch_size = len(obs)
-            (img_feats, can_feats), lengths = self.from_shortest_path()      # Image Feature (from the shortest path)
-            ctx = self.encoder(can_feats, img_feats, lengths)
+            obj_lens = None
+            if args.denseObj:
+                (img_feats, can_feats, obj_lens, obj_feats), lengths = self.from_shortest_path()
+
+                ctx = self.encoder(can_feats, img_feats, lengths, objMask=obj_mask, objFeat=obj_feats)
+            else:
+                (img_feats, can_feats), lengths = self.from_shortest_path()      # Image Feature (from the shortest path)
+                ctx = self.encoder(can_feats, img_feats, lengths)
+
+
         h_t = torch.zeros(1, batch_size, args.rnn_dim).cuda()
         c_t = torch.zeros(1, batch_size, args.rnn_dim).cuda()
         ctx_mask = utils.length2mask(lengths)
+
+
+
 
         # Get Language Input
         if insts is None:

r2r_src/utils.py
@@ -334,7 +334,7 @@ def read_obj_dense_features(dense_obj_feat1, dense_obj_feat2, bbox, sparse_obj_f
             viewpoint_object.append({'bbox':bb,'angles_h': angles_h, 'angles_e': angles_e,'features': features,
                                   'text': txt,'viewIndex': viewIndex,'prob': prob})
         if not flag and (th != 1):
-            viewpoint_object.append({'bbox':avg_b,'angles_h': angles_h, 'angles_e': angles_e, 'features': avg_feature,
+            viewpoint_object.append({'bbox':avg_b,'angles_h': avg_e, 'angles_e': avg_h, 'features': avg_feature,
                                   'text': 'average', 'viewIndex': None, 'prob': None})
             none_num +=1
         if th == 1:
@@ -375,7 +375,7 @@ def read_obj_dense_features(dense_obj_feat1, dense_obj_feat2, bbox, sparse_obj_f
     return objs
 
 def read_obj_sparse_features(sparse_obj_feat, th):
-    print("Start loading the object dense feature")
+    print("Start loading the object sparse feature")
     start = time.time()
     obj_s_feat = np.load(sparse_obj_feat, allow_pickle=True).item()
     viewpointIds = load_viewpointids()
@@ -679,6 +679,13 @@ def length2mask(length, size=None):
     size = int(max(length)) if size is None else size
     mask = (torch.arange(size, dtype=torch.int64).unsqueeze(0).repeat(batch_size, 1)
                 > (torch.LongTensor(length) - 1).unsqueeze(1)).cuda()
+    return mask
+
+def traj_length2mask(length, max_len, size=None):
+    batch_size = max_len
+    size = int(max(length)) if size is None else size
+    mask = (torch.arange(size, dtype=torch.int64).unsqueeze(0).repeat(batch_size, 1)
+            > (torch.LongTensor(length) - 1).unsqueeze(1)).cuda()
     return mask
 
 def average_length(path2inst):